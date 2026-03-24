b7fbbb0c9c0cc47d6129ab08c8fbaec19cd95588
omarupk33 <kkomar12334@gmail.com>
2026-03-24 20:46:33 +0300

    Traning on using React

---
+++ src/Card.jsx
@@ -0,0 +1,11 @@
+function Card(){
+return (
+    <div className='card'>
+        <img alt="Profile Pic"/>
+        <h2 className="card-title">Omar</h2>
+        <p className="  card-title">I am trying to hold together all of my hobbies</p>
+    </div>
+)
+}
+
+export default Card
