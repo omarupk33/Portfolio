3ddf6958b4b6641241f825817bf297e5247b2281
omarupk33 <kkomar12334@gmail.com>
2026-04-01 20:29:05 +0300

    more additional things

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,7 +1,24 @@
 function Header(){
-    return <p className="header">
-        Count me in Idiots!!
-    </p>
+    return <div className="header">
+       <div className="profile-section">
+        <div className="pic">
+            {/* Not rendering for some reason */}
+            <img src="assets/image.png" width='20px' height='auto'  alt="pic" ></img>
+        </div>
+        <h3 className="name">
+            Omar and Rayyan
+        </h3>
+       </div>   
+       
+       <div className="cv"><a href="#example">A link for CV</a>
+
+       </div>
+
+       <div className="about you">
+        <a href="#example">A link for about you page</a>
+       </div>
+
+    </div>
 }
 
 export default Header
